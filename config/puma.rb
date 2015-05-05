7701a692943704caf56b08cca873860e39ab2566
Max Holder <mxhold@gmail.com>
2015-05-05 10:49:18 -0400

    Update Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,6 @@
 # Change to match your CPU core count
 workers 1
 
-# Min and Max threads per worker
-threads 1, 6
-
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
